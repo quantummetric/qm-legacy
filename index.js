611bf7e31ce3c5220bf8b755996da7939247349a
Ryan Gasparini <ryan@rxgx.com>
2022-12-05 16:09:19 -0700

    feat: add display class and test file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const Display = require('./display.js');
 const Maybe = require('./maybe.js');
 const functionalUtil = require('./functional-util.js');
 const immutableOptics = require('./immutable-optics.js');
 const immutableUtil = require('./immutable-util.js');
 
 module.exports = {
+  Display,
   Maybe,
   functionalUtil,
   immutableOptics,
--- index.test.js
+++ index.test.js
@@ -3,6 +3,7 @@ const subject = require("./index.js");
 
 describe('index', () => {
     it('index exports modules', () => {
+        expect(subject.Display).to.be.a('function');
         expect(subject.Maybe).to.be.a('function');
         expect(subject.functionalUtil).to.be.a('object');
         expect(subject.immutableOptics).to.be.a('object');
