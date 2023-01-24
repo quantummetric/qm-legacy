87c2f8233f7499c211d4819bda5bdd43e60e9488
Ryan Gasparini <ryan@rxgx.com>
2023-01-23 21:32:20 -0800

    Update immutable-optics.test.js

--- immutable-optics.test.js
+++ immutable-optics.test.js
@@ -8,3 +8,27 @@ describe('immutable-optics', () => {
         expect(subject.preOrderTraversal).to.be.a("function");
     });
 });
+
+describe('traversal', () => {
+    it('should traverse the object and apply the function to each element', () => {
+        const obj = { a: { b: { c: 1 } } };
+        const traversalFn = (o) => [['a', 'b', 'c']];
+        const fn = (val, path) => val + 1;
+        const updatedObj = traversal(traversalFn)(fn)(obj);
+        expect(updatedObj).to.equal({ a: { b: { c: 2 } } });
+    });
+    it('should handle empty objects', () => {
+        const obj = {};
+        const traversalFn = (o) => [['a', 'b', 'c']];
+        const fn = (val, path) => val + 1;
+        const updatedObj = traversal(traversalFn)(fn)(obj);
+        expect(updatedObj).to.equal({});
+    });
+    it('should handle non-objects', () => {
+        const obj = 1;
+        const traversalFn = (o) => [['a', 'b', 'c']];
+        const fn = (val, path) => val + 1;
+        const updatedObj = traversal(traversalFn)(fn)(obj);
+        expect(updatedObj).to.equal(1);
+    });
+});
