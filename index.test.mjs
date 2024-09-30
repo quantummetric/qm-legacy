6b8a123006358fb034c8c33e2d1cd40301588ec6
Ryan Gasparini <ryan@rxgx.com>
2024-09-30 16:30:33 -0700

    fix: use moment-timezone and require with ext

--- index.test.mjs
+++ index.test.mjs
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import subject from './index.js';
+import decimalPlaces from './decimalPlaces.js';
 import formatBytes from './formatBytes.js';
 import formatCurrency from './formatCurrency.js';
 import formatDuration from './formatDuration.js';
 import formatNumber from './formatNumber.js';
+import humanizeDuration from './humanizeDuration.js';
 import prefixWith from './prefixWith.js';
 import suffixWith from './suffixWith.js';
 
@@ -25,6 +27,15 @@ describe('Display', () => {
         expect(suffixWith()).to.be.a("function");
     })
 
+    it('format decimal places', () => {
+        expect(decimalPlaces(0)(.01)).to.eq('0');
+        const formatter = decimalPlaces(2);
+        expect(formatter(123)).to.eq("123.00");
+        expect(formatter(123.4)).to.eq("123.40");
+        expect(formatter(123.456)).to.eq("123.46");
+        expect(formatter(123.4567)).to.eq("123.46");
+    })
+
     it('formats positive numbers', () => {
         expect(formatCurrency(123.45)).to.eq("$123.45");
         expect(formatCurrency(123.456, "£")).to.eq("£123.46");
@@ -35,6 +46,20 @@ describe('Display', () => {
         expect(formatCurrency(-123.45)).to.eq("$-123.45");
         expect(formatCurrency(-123.456, "£")).to.eq("£-123.46");
         expect(formatCurrency(-123.4567, "€")).to.eq("€-123.46");
+    })
+
+    it('formats duration', () => {
+        expect(formatDuration()(100)).to.eq("0.10s");
+        expect(formatDuration()(2300)).to.eq("2s");
+        expect(formatDuration()(0)).to.eq("0s");
+    })
+
+    it('formats duration', () => {
+        expect(humanizeDuration()(100)).to.eq("100 milliseconds");
+        expect(humanizeDuration(true)(100)).to.eq("100 ms");
+        expect(humanizeDuration()(2300)).to.eq("2.3 seconds");
+        expect(humanizeDuration(true, 2)(2300)).to.eq("2.30 s");
+        expect(humanizeDuration()(0)).to.eq("0 millisecond");
     })
 
     it('creates a prefix function', () => {
